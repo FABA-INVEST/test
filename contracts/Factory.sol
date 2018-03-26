a64b23c6993507d5a41695737fb2c8e3a71f0b64
Robert Muller <robert.muller@seznam.cz>
2018-03-27 00:25:57 +0200

    Modified Factory - with contracts list

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -4,18 +4,34 @@ import 'contracts/FABA_Token1.sol';
 
 contract Factory {
 
+  address[] public contracts;
+
+  function getContractCount() 
+    public
+    constant
+    returns(uint contractCount)
+  {
+    return contracts.length;
+  }
+
+
+
     function createContract(
         address _fundsWallet,
         uint256 _startTimestamp,
         uint256 _minCapEth,
-        uint256 _maxCapEth) returns(address created) 
+        uint256 _maxCapEth) 
+        public
+        returns(address created) 
     {
-        return new BeanToken1(
+        BeanToken1 bt = new BeanToken1(
             _fundsWallet,
             _startTimestamp,
             _minCapEth * 1 ether,
             _maxCapEth * 1 ether
         );
+        contracts.push(bt);
+        return bt;
     }
 }
 
